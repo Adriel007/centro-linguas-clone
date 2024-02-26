0fad8b636d8eab3ded1d47f71c833f57697b1a0c
Buletiss <vinicius1997feitosa@gmail.com>
2024-02-26 13:00:03 -0300

    feat: :sparkles: adição da rotas para a nota e troca  para a porta 3333

--- server.js
+++ server.js
@@ -5,16 +5,18 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081"
+  origin: "http://localhost:8081",
 };
 
 app.use(cors(corsOptions));
 
 // parse requests of content-type - application/json
-app.use(express.json());  /* bodyParser.json() is deprecated */
+app.use(express.json()); /* bodyParser.json() is deprecated */
 
 // parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));   /* bodyParser.urlencoded() is deprecated */
+app.use(
+  express.urlencoded({ extended: true })
+); /* bodyParser.urlencoded() is deprecated */
 
 const db = require("./app/models");
 db.sequelize.sync();
@@ -30,9 +32,10 @@ app.get("/", (req, res) => {
 
 // require("./app/routes/turorial.routes")(app);
 require("./app/routes/aluno.routes")(app);
+require("./app/routes/notas.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
