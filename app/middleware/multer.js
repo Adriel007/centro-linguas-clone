44c24336475d13a2c3db827a98ded6df0bf684c1
Adriel007 <adrielsouzaandrade8@gmail.com>
2025-01-02 16:47:53 -0300

    feat: - Apenas novas extensões aceitas de upload

--- app/middleware/multer.js
+++ app/middleware/multer.js
@@ -1,38 +1,41 @@
-const multer = require('multer');
-const path = require('path');
-const fs = requite('fs');
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
 
-//funÁao pra criar os subdiretorios com base no tipo de usuario e o tipo de arquivo
+//funÔøΩao pra criar os subdiretorios com base no tipo de usuario e o tipo de arquivo
 const armazenamento = multer.diskStorage({
-    destination: (req, file, cb) => {
-        const userType = req.params.userType; //tipo aluno, professor, administrador
-        const fileType = req.params.fileType; //tipo tarefas, materiais didaticos, certificados, etc
+  destination: (req, file, cb) => {
+    const userType = req.params.userType; //tipo aluno, professor, administrador
+    const fileType = req.params.fileType; //tipo tarefas, materiais didaticos, certificados, etc
 
-        const dir = `uploads/${userType}/${fileType}`;
-        
-        fs.mkdirSync(dir, { recursive: true });//cria o diretorio, caso nao exista
+    const dir = `uploads/${userType}/${fileType}`;
 
-        cb(null, dir);//define o diretorio de destino para o arquivo
-    },
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + path.extname(file.originalname));//nome do arquivo com horario
-    }
+    fs.mkdirSync(dir, { recursive: true }); //cria o diretorio, caso nao exista
+
+    cb(null, dir); //define o diretorio de destino para o arquivo
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname)); //nome do arquivo com horario
+  },
 });
 
-//funÁao para filtrar e limitar o tamanho do arquivo permitido
+//funÔøΩao para filtrar e limitar o tamanho do arquivo permitido
 const upload = multer({
-    storage: armazenamento,
-    limits: { fileSize: 100 * 1024 * 1024 },//limite de 100mb
-    fileFilter: (req, file, cb) => {
-        const allowedTypes = /jpeg|jpg|pdf|docx|ppt|zip/;
-        const ext = allowedTypes.test(path.extname(file.originalname).toLowerCase());
-        const mimetype = allowedTypes.test(file.mimetype);
+  storage: armazenamento,
+  limits: { fileSize: 100 * 1024 * 1024 }, //limite de 100mb
+  fileFilter: (req, file, cb) => {
+    const allowedTypes =
+      /jpeg|jpg|pdf|docx|ppt|zip|doc|xlsx|xls|txt|csv|mp3|mp4|wav|ogg|png|gif|pptx/;
+    const ext = allowedTypes.test(
+      path.extname(file.originalname).toLowerCase()
+    );
+    const mimetype = allowedTypes.test(file.mimetype);
 
-            if (ext && mimetype) {
-                cb(null, true);
-            } else {
-                cb(new Error('Esse tipo de arquivo n„o È permitido.'));
-            }
+    if (ext && mimetype) {
+      cb(null, true);
+    } else {
+      cb(new Error("Esse tipo de arquivo n√£o √© permitido."));
     }
+  },
 });
 module.exports = upload;
