9898bc02a84dfe39d77b084e6d60f7860b1616f3
Adriel007 <adrielsouzaandrade8@gmail.com>
2025-01-02 16:53:13 -0300

    chore: - Nada, so tirei oc omentario, indentou sozinho tambem

--- app/models/arquivos.model.js
+++ app/models/arquivos.model.js
@@ -1,32 +1,31 @@
-//arquivo model FALTA CRIAR A TABELA ARQUIVOS NO DB
 module.exports = (sequilize, DataTypes) => {
-    const Arquivo = sequilize.define('Arquivos', {
-        nome: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        tamanho: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        },
-        tipo: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        dataUpload: {
-            type: DataTypes.DATE,
-            allowNull: false,
-            defaultValue: DataTypes.NOW,
-        },
-        usuarioId: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            references: {
-                model: 'tb_usuario',
-                key: 'id',
-            },
-        },
-    });
+  const Arquivo = sequilize.define("tb_arquivos", {
+    nome: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    tamanho: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    tipo: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    dataUpload: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    usuarioId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "tb_usuario",
+        key: "id",
+      },
+    },
+  });
 
-return Arquivos;
+  return Arquivo;
 };
